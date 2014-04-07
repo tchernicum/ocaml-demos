abe14d7e825647f6e1fd6aa8865d7daa9001294c
Vladimir Brankov <brankovv@gmail.com>
2014-04-07 06:13:07 -0400

    Some changes.

--- intrin/test.c
+++ intrin/test.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "emmintrin.h"
 
-void main()
+int main()
 {
-  __m64 x, y;
-  scanf("%ld %ld", (long int*) &x, (long int*) &y);
-  __m128i z = _mm_set_epi64(x, y);
+  long long x, y, z, w;
+  __m128i a;
+  if (scanf("%lld %lld", &x, &y) != 2)
+    return EXIT_FAILURE;
 
-  printf("%ld\n", (long int) x);
+  z = x + y;
+  w = x - y;
+  a = _mm_set_epi64x(z, w);
+  a = _mm_add_epi64(a, a);
+
+  printf("%lld\n", _mm_cvtsi128_si64(a));
+
+  return EXIT_SUCCESS;
 }
