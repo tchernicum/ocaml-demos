b0279fa7c0ed21bcccf7e01b0c2873b252399477
Vladimir Brankov <vbrankov@janestreet.com>
2015-04-07 12:06:58 -0400

    Fix the example.

--- intrin/string_index.ml
+++ intrin/string_index.ml
@@ -1,30 +1,25 @@
 (* The benchmark shows 4-5x faster [String.index] using hardware primitives. *)
 
 type m128i
-
 type ptr
-external int_as_pointer : int -> ptr = "%int_as_pointer"
-external lsla : ptr -> int -> ptr = "%lsla"
-external adda : string -> ptr -> ptr = "%adda"
 
+external index : string -> int -> ptr = "%asm" ""
+       "sar	%2
+	add	%0, %2" "r" "2" "=r"
 external _mm_loadu_si128 : ptr -> m128i = "%asm" ""
-       "movdqu	(%0), %1" "r" "=x"
-let offset s i = adda s (lsla (int_as_pointer i) 3)
-let _mm_loadu_si128 s i = _mm_loadu_si128 (offset s i)
-
+       "movdqu	(%0), %1	# _mm_loadu_si128" "r" "=x"
 external _mm_set1_epi64x : int64 -> m128i = "%asm" ""
        "movq	%0, %1" "r" "=x"
-
 external _mm_pcmpestri : m128i -> int64 -> m128i -> int64 -> nativeint -> int64 =
   "%asm" "" "pcmpestri	%4, %2, %0" "x" "a" "mx" "d" "i" "=c"
 
 let rec string_index_rec s l p c =
   let cc = _mm_set1_epi64x (Int64.of_int (Char.code c)) in
-  let a = _mm_loadu_si128 s p in
+  let a = _mm_loadu_si128 (index s p) in
   let r = _mm_pcmpestri cc 1L a (Int64.of_int l) 0n in
-  if r < Int64.of_int 16 then p * 16 + Int64.to_int r
+  if r < 16L then p + Int64.to_int r
   else if l < 16 then -1
-  else string_index_rec s (l - 16) (p + 1) c
+  else string_index_rec s (l - 16) (p + 16) c
 
 let string_index s c =
   string_index_rec s (String.length s) 0 c
