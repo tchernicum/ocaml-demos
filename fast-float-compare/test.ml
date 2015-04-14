54b0d5fc19076c56f771bbdd30b75e6249f2ea42
Vladimir Brankov <vbrankov@janestreet.com>
2015-04-14 15:23:24 -0400

    Update the float comparison benchmark.

--- fast-float-compare/test.ml
+++ fast-float-compare/test.ml
@@ -1,15 +1,3 @@
-external float_compare_old : float -> float -> int = "caml_float_compare_old"
-
-let () =
-  let a = Array.init 100 (fun _ -> Random.float 100. -. 50.) in
-  List.iteri
-    (fun i x -> a.(i) <- x)
-    [0.; 1.; -1.; nan; neg_infinity; infinity; max_float; -. max_float;
-     2. ** -1074.; -. 2. ** -1074.];
-  Array.iter (fun x ->
-    Array.iter (fun y ->
-      assert (compare x y = float_compare_old x y)) a) a
-
 let a = Array.init 1_000_001 (fun _ -> Random.float 100.)
 let b = Array.init 1_000_001 float_of_int
 
@@ -34,23 +22,7 @@ let () =
       let _y = Array.unsafe_get a (i + 1) in
       s := !s + 1
     done);
-  bench "compare_old unpredictable x 1M" (fun () ->
-    let a = a in
-    let s = ref 0 in
-    for i = 0 to Array.length a - 2 do
-      let x = Array.unsafe_get a i in
-      let y = Array.unsafe_get a (i + 1) in
-      s := !s + float_compare_old x y
-    done);
-  bench "compare_old   predictable x 1M" (fun () ->
-    let b = b in
-    let s = ref 0 in
-    for i = 0 to Array.length b - 2 do
-      let x = Array.unsafe_get b i in
-      let y = Array.unsafe_get b (i + 1) in
-      s := !s + float_compare_old x y
-    done);
-  bench "compare     unpredictable x 1M" (fun () ->
+  bench "compare unpredictable x 1M" (fun () ->
     let a = a in
     let s = ref 0 in
     for i = 0 to Array.length a - 2 do
@@ -58,7 +30,7 @@ let () =
       let y = Array.unsafe_get a (i + 1) in
       s := !s + compare x y
     done);
-  bench "compare       predictable x 1M" (fun () ->
+  bench "compare   predictable x 1M" (fun () ->
     let b = b in
     let s = ref 0 in
     for i = 0 to Array.length b - 2 do
